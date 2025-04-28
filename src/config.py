0636f457843e75481d12a0fb14251b9b5bc5c73f
Hagzilla <d.mascheix@gmail.com>
2025-04-29 00:03:38 +0200

    Add JWT expiration configuration to centralized settings

--- src/config.py
+++ src/config.py
@@ -7,3 +7,4 @@ load_dotenv()
 # Centralized configuration variables
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
+JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES", 30))  # Default to 30 minutes
