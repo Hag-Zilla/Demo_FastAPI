f7c15c3931aa0b1ae01a0f23005e35bffc3d90b7
Hagzilla <d.mascheix@gmail.com>
2025-05-04 23:21:27 +0200

    Fix default value for SECRET_KEY in centralized configuration

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 # Centralized configuration variables
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")  # Default
+ALGORITHM = os.getenv("ALGORITHM")
 JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES", 30))  # Default to 30 minutes
